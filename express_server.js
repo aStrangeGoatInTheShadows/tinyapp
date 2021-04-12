befefbadd52562b696ddaa98f6dc20de8edbc27e
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-12 23:32:39 +0000

    Can now add new tiny urls and redirect to them

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const parseJson = require('parse-json');
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -15,11 +16,27 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+// Logic for making new urls
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const randomString = generateRandomString();
+  if(req.body.longURL.startsWith('http')){
+    urlDatabase[randomString] = req.body.longURL;
+  } else {
+    urlDatabase[randomString] = `http://${req.body.longURL}`;
+  }
+  
+  console.log(`${randomString} now links too ${req.body.longURL}`);  // Log the POST request body to the console
+  res.send('added to database');   
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log (`User redirected to ${longURL}`)
+  res.redirect(longURL);
+});
+
+// Page for making new urls
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -28,7 +45,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -40,14 +56,14 @@ app.listen(PORT, () => {
 
 // Generously donated by https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function generateRandomString() {
-  let result           = [];
-  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = [];
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
   const stringLength = 6;
 
 
-  for ( var i = 0; i < stringLength; i++ ) {
+  for (var i = 0; i < stringLength; i++) {
     result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
- }
- return result.join('');
+  }
+  return result.join('');
 }
