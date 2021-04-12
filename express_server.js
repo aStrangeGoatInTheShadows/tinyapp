50dbc52af43fcb9cd82d20a6559367fdd4c0e0e2
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-12 22:46:40 +0000

    middle of implementation of generating new tinyURLs

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +13,11 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
@@ -31,4 +38,16 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// Generously donated by https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function generateRandomString() {
+  let result           = [];
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  const stringLength = 6;
 
+
+  for ( var i = 0; i < stringLength; i++ ) {
+    result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+ }
+ return result.join('');
+}
