4bf28fb9c83071d8a5d51eeb01637691bf2ba100
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-13 21:43:48 +0000

    Added login and logout functionality with cookies

--- express_server.js
+++ express_server.js
@@ -1,9 +1,12 @@
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,20 +14,43 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies.username
+  };
+
   res.render("urls_index", templateVars);
 });
 
 // Sends a blank page to index
 app.get("/", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  
   res.render("urls_index", templateVars);
 });
+
+//Creates a cookie to save the user
+app.post(`/login`, (req, res) => {
+  res.cookie('username', req.body.username);
+
+  res.redirect("/urls");
+});
+
+// Clears the cookie to logout the user
+app.post(`/logout`, (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
+
 
 // deletes a data base entry for a TinyURL
 app.post(`/urls/:shortURL/delete`, (req, res) => {
   console.log(`${res.shortURL}'s link to ${urlDatabase[res.shortURL]} has been deleted.`);
-  
+
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
@@ -60,7 +86,12 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
+
   res.render("urls_show", templateVars);
 });
 
